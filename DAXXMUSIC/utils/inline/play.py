0c37119218babd3acc2d411e7070616829f61204
Mr-Sakil <142120431+Mr-Sakil@users.noreply.github.com>
2024-06-21 15:36:46 +0530

    Add files via upload

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -1,8 +1,8 @@
 import math
 from pyrogram.types import InlineKeyboardButton
-from DAXXMUSIC import app
+from VipMusic import app
 import config
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from VipMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "⚘—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—⚘————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——⚘———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———⚘——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————⚘—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————⚘————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————⚘———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————⚘——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————⚘—"
     else:
-        bar = "—————————◉"
+        bar = "—————————⚘"
     buttons = [
                 [
             InlineKeyboardButton(
@@ -90,11 +90,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"VipPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"VipPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
